1d674ce11ecf057349f928f5ef1c56dfec60a392
Soeren Magnus Olesen <soeren@molesen.ch>
2025-10-25 20:05:05 +0200

    test: Add unit tests for GoWaitGroupExtensions with cancellation support
    Signed-off-by: Soeren Magnus Olesen <soeren@molesen.ch>

---
+++ tests/Hugo.Tests/Go/General/GoWaitGroupExtensionsTests.cs
@@ -0,0 +1,84 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace Hugo.Tests;
+
+public class GoWaitGroupExtensionsTests
+{
+    [Fact]
+    public void Go_ShouldThrow_WhenWaitGroupNull() =>
+        Assert.Throws<ArgumentNullException>(() => GoWaitGroupExtensions.Go(null!, () => Task.CompletedTask));
+
+    [Fact]
+    public void Go_ShouldThrow_WhenFuncNull() =>
+        Assert.Throws<ArgumentNullException>(() => GoWaitGroupExtensions.Go(new WaitGroup(), (Func<Task>)null!));
+
+    [Fact]
+    public async Task Go_ShouldRunFunctionAndTrackWaitGroup()
+    {
+        var wg = new WaitGroup();
+        var counter = 0;
+
+        GoWaitGroupExtensions.Go(wg, async () =>
+        {
+            await Task.Yield();
+            Interlocked.Increment(ref counter);
+        });
+
+        await wg.WaitAsync(TestContext.Current.CancellationToken);
+
+        Assert.Equal(1, counter);
+        Assert.Equal(0, wg.Count);
+    }
+
+    [Fact]
+    public void Go_WithCancellationToken_ShouldThrow_WhenWaitGroupNull() =>
+        Assert.Throws<ArgumentNullException>(() => GoWaitGroupExtensions.Go(null!, _ => Task.CompletedTask, CancellationToken.None));
+
+    [Fact]
+    public void Go_WithCancellationToken_ShouldThrow_WhenFuncNull() =>
+        Assert.Throws<ArgumentNullException>(() => GoWaitGroupExtensions.Go(new WaitGroup(), (Func<CancellationToken, Task>)null!, CancellationToken.None));
+
+    [Fact]
+    public async Task Go_WithCancellationToken_ShouldPassTokenAndComplete()
+    {
+        var wg = new WaitGroup();
+        using var cts = new CancellationTokenSource();
+        var observed = new List<CancellationToken>();
+
+        GoWaitGroupExtensions.Go(wg, async token =>
+        {
+            observed.Add(token);
+            await Task.Yield();
+        }, cts.Token);
+
+        await wg.WaitAsync(TestContext.Current.CancellationToken);
+
+        var token = Assert.Single(observed);
+        Assert.Equal(cts.Token, token);
+        Assert.Equal(0, wg.Count);
+    }
+
+    [Fact]
+    public async Task Go_WithCancellationToken_ShouldHandlePreCanceledToken()
+    {
+        var wg = new WaitGroup();
+        using var cts = new CancellationTokenSource();
+        cts.Cancel();
+        var invoked = false;
+
+        GoWaitGroupExtensions.Go(wg, token =>
+        {
+            invoked = true;
+            return Task.CompletedTask;
+        }, cts.Token);
+
+        await wg.WaitAsync(TestContext.Current.CancellationToken);
+
+        Assert.False(invoked);
+        Assert.Equal(0, wg.Count);
+    }
+}
